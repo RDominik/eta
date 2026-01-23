2e12950015243b28a251ad2433782bbd49681b19
rieplD <riepl.dominik@gmx.de>
2026-01-23 21:32:10 +0100

    wallbox control integration

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import asyncio
 import time
 import schedule
 from goE import goEcontrol
+from goE import wallbox_control
 
 RERUN_TIME = 5  # seconds
 # inverter_data = {
@@ -27,6 +28,7 @@ async def call_inverter():
     global inverter_data
     try:
         inverter_data = await readGoodwe.readInverter(inverter)
+        wallbox_control.get_inverter_data(inverter_data)
         print("inverter measurement finished ")
     except Exception as e:
         print(f"Error reading inverter data: {e}")
@@ -35,22 +37,23 @@ async def call_inverter():
 
 def call_wallbox():
     try:
-        goEcontrol.load_control(inverter_data)
+        wallbox_control.wallbox_control()
+        # goEcontrol.load_control(inverter_data)
         print("wallbox control finished ")
     except Exception as e:
         print(f"Error calling wallbox: {e}")
         time.sleep(10)
 
-def wallbox_mean_calculation():
-    try:
-        goEcontrol.mean_calculation(inverter_data)
-    except Exception as e:
-        print(f"Error mean calculation wallbox: {e}")
-        time.sleep(10)
+# def wallbox_mean_calculation():
+#     try:
+#         goEcontrol.mean_calculation(inverter_data)
+#     except Exception as e:
+#         print(f"Error mean calculation wallbox: {e}")
+#         time.sleep(10)
     
 #schedule.every(5).seconds.do(call_inverter)
-schedule.every(60).seconds.do(call_wallbox)
-schedule.every(5).seconds.do(wallbox_mean_calculation)
+schedule.every(10).seconds.do(call_wallbox)
+# schedule.every(5).seconds.do(wallbox_mean_calculation)
 
 async def main():
     global inverter_data
