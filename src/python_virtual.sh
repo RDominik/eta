5f8cb274cb926fdb759670bc66a60c6f7922c6c6
rieplD <riepl.dominik@gmx.de>
2026-01-25 21:30:30 +0100

    remove python script name and add comments

--- src/python_virtual.sh
+++ src/python_virtual.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-# Einstellungen
+# set virtual environment name
 VENV_DIR="venv"
-PYTHON_SCRIPT="/home/dominik/repositorys/goodwe/eta/src/goodWe_influx.py"
 
-# Virtuelle Umgebung erstellen, falls sie nicht existiert
+# create virtual environment if not exists
 if [ ! -d "$VENV_DIR" ]; then
-    echo "ðŸ“¦ Erstelle virtuelle Umgebung..."
+    echo "create virutal environment..."
     python3 -m venv "$VENV_DIR"
 fi
 
-# Umgebung aktivieren
+# activate virtual environment
 source "$VENV_DIR/bin/activate"
 
-export INFLUX_TOKEN="CKicKENZvt3Zhu2hPBABenhNR2IttRwuT0cfCFB2rRp8ZnttuZhOprInAJe0jRfOKPXMvBQymJGbfokZQ8avTA=="
-
-# Pakete installieren (falls noch nicht installiert)
-echo "ðŸ“¥ Installiere benÃ¶tigte Python-Pakete..."
+# package installation
+echo "Python package installation..."
 pip install --upgrade pip
-pip install goodwe influxdb-client
+pip install goodwe influxdb-client paho-mqtt
 
-# Starte dein Python-Skript
-echo "ðŸš€ Starte das Python-Skript..."
-python3 "$PYTHON_SCRIPT"
+
