8cfe16dcc348338b9752ae565b54ecd1e3aa58f9
Dominik <RDominik@users.noreply.github.com>
2026-02-13 09:56:27 +0000

    merge(webui): resolve conflicts; add EnergyFlow route and nav

--- webui/src/App.jsx
+++ webui/src/App.jsx
@@ -21,6 +21,7 @@ export default function App() {
           <NavLink to="/wallbox" style={linkStyle}>Wallbox</NavLink>
           <NavLink to="/inverter" style={linkStyle}>Wechselrichter</NavLink>
           <NavLink to="/energy" style={linkStyle}>PV Energiefluss</NavLink>
+          <NavLink to="/heating" style={linkStyle}>ETA Heizung</NavLink>
         </nav>
         <div style={{ marginTop: 24, fontSize: 12, color: '#666' }}>
           <div>v0.1 (Preview)</div>
